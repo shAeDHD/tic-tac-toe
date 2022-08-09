99c3fc8979388ddb0f6974a079e4338038949004
Shae Riches <shaeriches@gmail.com>
2022-08-09 11:57:51 +1000

    tues: push: 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 console.log('main.js connected')
+let playerTurn = 'playerTwo';
 
- 
+//  function to alternate between players on square click.
+const switchPriority = function ( ) {
 
-const checkIfSquareOccupied = { 
-    a1  : false,
+    if ( playerTurn === 'playerOne' ) {
+
+        playerTurn = 'playerTwo'
+
+    } else {
+        playerTurn = 'playerOne'
+
+    }    
+
+
+}; //       closes swithPriority
+
+//  object containing grid references for game board. 
+const gameBoard = { 
+    a1  : false,  
     a2  : false,
     a3  : false,
     b1  : false,
@@ -12,159 +27,60 @@ const checkIfSquareOccupied = {
     c1  : false,
     c2  : false,
     c3  : false,
-}
+}; //       closes gameBoard
 
+
+
+
+
+// eventListener that activates when a grid square is selected.
 $('.playingSquare').on('click', function (  ) {
     // console.log('$a1 clicked');     //      check = true 
     console.log(this);                 //      check = true
+
+    //      create new variable to house the #id of 'this' playingSquare.
     const id = $(this).attr('id');
 
-    if ( checkIfSquareOccupied[id] === false  ) {
-        $('.playingSquare').css("backgroundColor", "rgba(255, 0, 0, 0)")
-        checkIfSquareOccupied[id] = true;
+    if ( gameBoard[id] === false && playerTurn === 'playerOne' ) {
+        // reset opacity of background color in all .playingSquare to 0. 
+        $('.playingSquare').css("backgroundColor", "rgba(255, 0, 0, 0)")   
+
+        // create newVariable of X image and then append to the current node.
         const $XImage = $('<img src="images/plain X.png">');
         $XImage.addClass('placedCharacters');
         $(this).append($XImage);
 
-        console.log(checkIfSquareOccupied) //    check = true
+        gameBoard[id] = 'O';
+        console.log(gameBoard); //    check = true
     
+        switchPriority();
+        console.log(playerTurn); //     check = true
+        
+
+    } else if (gameBoard[id] === false && playerTurn === 'playerTwo'  ) {
+
+        // reset opacity of background color in all .playingSquare to 0. 
+        $('.playingSquare').css("backgroundColor", "rgba(255, 0, 0, 0)")   
+
+        // create newVariable of O image and then append to the current node.
+        const $OImage = $('<img src="images/plain O.png">');
+        $OImage.addClass('placedCharacters');
+        $(this).append($OImage);
+
+        gameBoard[id] = 'O';
+        console.log(gameBoard); //    check = true
+    
+        switchPriority();
+        console.log(playerTurn); //     check = true    
+
+
 
     } else {
 
+        // change opacity of background red color to 1. 
         $(this).css("backgroundColor", "rgba(255, 0, 0, 1)")
 
-
-    } 
-
-
-});
+    } //        closes if/else  
 
 
-
-// $('#a1').on('click', function ( ) {
-//     // console.log('$a1 clicked');     //      check = true 
-//     const $XImage = $('<img src="images/plain X.png">');
-//     $XImage.addClass('placedCharacters');
-//     $('#a1').append($XImage);
-//     checkIfSquareOccupied.a1 = true;
-//     // console.log(checkIfSquareOccupied.a1);       //      check = true
-
-// })
-
-
-// gridSquares.$a1.on('click', function ( ) {
-//     console.log('$a1 clicked');     //      check = true 
-//     const $OImage = $('<img src="images/plain O.png">');
-//     $OImage.addClass('placedCharacters');
-//     $('#a1').append($OImage);
-//     console.log($OImage);       //      check = true
-
-// })
-// gridSquares.$a2.on('click', function ( ) {
-//     console.log('$a2 clicked');     //      check = true
-//     const $OImage = $('<img src="images/plain O.png">')
-//     $OImage.addClass('placedCharacters');
-//     $('#a2').append($OImage);
-//     console.log($OImage);
-// })
-// gridSquares.$a3.on('click', function ( ) {
-//     console.log('$a3 clicked');     //      check = true
-//     const $OImage = $('<img src="images/plain O.png">')
-//     $OImage.addClass('placedCharacters');
-//     $('#a3').append($OImage);
-//     console.log($OImage);
-// })
-// gridSquares.$b1.on('click', function ( ) {
-//     console.log('$b1 clicked');     //      check = true
-//     const $OImage = $('<img src="images/plain O.png">')
-//     $OImage.addClass('placedCharacters');
-//     $('#b1').append($OImage);
-//     console.log($OImage);
-// })
-// gridSquares.$b2.on('click', function ( ) {
-//     console.log('$b2 clicked');     //      check = true
-//     const $OImage = $('<img src="images/plain O.png">')
-//     $OImage.addClass('placedCharacters');
-//     $('#b2').append($OImage);
-//     console.log($OImage);
-// })
-// gridSquares.$b3.on('click', function ( ) {
-//     console.log('$b3 clicked');     //      check = true
-//     const $OImage = $('<img src="images/plain O.png">')
-//     $OImage.addClass('placedCharacters');
-//     $('#b3').append($OImage);
-//     console.log($OImage);
-// })
-// gridSquares.$c1.on('click', function ( ) {
-//     console.log('$c1 clicked');     //      check = true
-//     const $OImage = $('<img src="images/plain O.png">')
-//     $OImage.addClass('placedCharacters');
-//     $('#c1').append($OImage);
-//     console.log($OImage);
-
-// })
-// gridSquares.$c2.on('click', function ( ) {
-//     console.log('$c2 clicked');     //      check = true
-//     const $OImage = $('<img src="images/plain O.png">')
-//     $OImage.addClass('placedCharacters');
-//     $('#c2').append($OImage);
-//     console.log($OImage);
-
-// })
-// gridSquares.$c3.on('click', function ( ) {
-//     console.log('$c3 clicked');     //      check = true
-//     const $OImage = $('<img src="images/plain O.png">')
-//     $OImage.addClass('placedCharacters');
-//     $('#c3').append($OImage);
-//     console.log($OImage);       //      check = true
-// })
-
-
-
-// PSEUDO-CODE for GAME LOGIC
-
-
-// there are two players, either player one or player two goes first.
-    // for first draft, first person to click will go first. 
-        // in future draft, random number generator to determine. 
-
-// when one player clicks on a square, they place their symbol in that square - then it becomes the other players turn.
-    //  the first player to get '3 in a row' wins. 
-    //  cannot overide other players placement.
-
-// const squareSelected = function (  ) {
-    
-//     gridSquares = { 
-//         $a1  : $("#a1"),
-//         $a2  : $("#a2"),
-//         $a3  : $("#a3"),
-//         $b1  : $("#b1"),
-//         $b2  : $("#b2"),
-//         $b3  : $("#b3"),
-//         $c1  : $("#c1"),
-//         $c2  : $("#c2"),
-//         $c3  : $("#c3"),
-//     };
-//     if 
-
-
-// };
-
-
-// const playerOneChooses = function ( squareSelected, ) {
-    
-
-// };
-
-// const playerTwoChooses = function (squareSelected, ) {
-
-
-// };
-
-// const threeInARow = function (){
-//     if ( df ) {
-
-//     }
-
-
-// };
+}); //      closes .onSquareClick
