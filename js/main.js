59d27f2987aef46fa7868963e65db0359b48ffe4
Shae Riches <shaeriches@gmail.com>
2022-08-12 15:03:25 +1000

    tidy

--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
 console.log('main.js connected')
-//              globalVariables
+/////////////////////////////////////////////|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//////////////////////////////////////|globalVariables|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////////////////////////////////////|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 let playerWithPriority = 'playerOne';
 let gameWinner = null;
 let turnCounter = 0;
+let gameRulesShowing = true
 
-//  function to alternate between players on square click.
-const switchPriority = function ( ) {                   //              swithPriority 
-
-    if ( playerWithPriority === 'playerTwo' ) {
-
-        playerWithPriority = 'playerOne'
-
-    } else {
-        playerWithPriority = 'playerTwo'
-
-    }    
-
-
-};                                                      //       closes swithPriority
-
+/////////////////////////////////////////////|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//////////////////////////////////////|dataStructures|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////////////////////////////////////|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 //  object containing grid references for game board. 
 const gameBoard = {                                     //              gameBoard
     a1  : false,  
@@ -45,6 +36,10 @@ const winCondition = [                                  //              winCondi
     [ 'a3' , 'b2' , 'c1' ],
 
 ];                                                      //       closes winCondition
+
+/////////////////////////////////////////////|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+////////////////////////////////////////|functions|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////////////////////////////////////|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 //  function to checks if player with priority has won.
 const checkWin = function (  ) {                        //              checkWin
@@ -141,55 +136,73 @@ const playerImageGenerator = function ( ) {             //              playerIm
 
 };                                                      //       closes playerImageGenerator
 
+//  function to alternate between players.
+const switchPriority = function ( ) {                   //              swithPriority 
+
+    if ( playerWithPriority === 'playerTwo' ) {
+
+        playerWithPriority = 'playerOne'
+
+    } else {
+        playerWithPriority = 'playerTwo'
+
+    }    
+
+
+};                                                      //       closes swithPriority
+
+
+/////////////////////////////////////////////|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//////////////////////////////////////|eventListeners|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+/////////////////////////////////////////////|\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
 // eventListener that activates when a grid square is clicked.
-$('.playingSquare').on('click', function ( ) {          //              onSquareClick eventListener
-    console.log(this);                 //      check = true
+$('.playingSquare').on('click', function ( ) {          //              
 
     // create new variable to house the #id of 'this'(*clicked*) playingSquare.
     const id = $(this).attr('id');
-    // console.log(id); //       check = true
-
 
     // create new variable to house the node of current Turn Log
-    const $turnIdAsArray             = $('.pLogTurn') ;
-    // console.log($turnIdAsArray); //       check = true
-    const $currentGameLogTurnAsNode    = $turnIdAsArray.eq(turnCounter) ;
-    // console.log($currentGameLogTurnAsNode); //       check = true
+    const $turnIdAsArray                = $('.pLogTurn')                 ;
+    const $currentGameLogTurnAsNode     = $turnIdAsArray.eq(turnCounter) ;
 
-
+    // if/else conditionals to determine if playerOne or playerTwo
     if ( gameBoard[id] === false && playerWithPriority === 'playerOne' ) {
 
+        // text generated for gameLog 
         let pOneTextLog = `On turn ${turnCounter}, Bill, played into square ${id}...`;
 
+        // find and change 'p' of gameTurn:Log 
         $currentGameLogTurnAsNode.find('p').html(pOneTextLog)
         $currentGameLogTurnAsNode.css('zIndex','251').show()
-        console.log('loaded');
 
         // reset opacity of background color in all .playingSquare to 0. 
         $('.playingSquare').css("backgroundColor", "rgba(255, 0, 0, 0)")   
 
-        // create newVariable of X image and then append to the current node.
+        // create newVariable of X image , then append to the current node.
         const playerOneSymbol = playerImageGenerator();
         $(this).append(playerOneSymbol);
         
         // set gameBoard reference to occupied by playerOne.
         gameBoard[id] = 'X';
-        console.log(gameBoard); //    check = true
+        // console.log(gameBoard); //    check = true
         
-        // gameLogTextForCurrentTurn()
-
-        // checks for win condition - if no winner, switch player priority from playerOne to playerTwo.
+        // checkWin function: if no winner, switch player priority
         checkWin();
-        console.log(playerWithPriority); //     check = true
+        // console.log(playerWithPriority); //     check = true
+        // adds 1 to turnCounter
         turnCounter++ ;
 
-    } else if (gameBoard[id] === false && playerWithPriority === 'playerTwo'  ) {
+        
+    } else if ( gameBoard[id] === false && playerWithPriority=== 'playerTwo' ) {
 
+        // text generated for gameLog 
         let pTwoTextLog = `On turn ${turnCounter}, Mr. Cage, played into square ${id}...`;
 
+        // find and change 'p' of gameTurn:Log 
         $currentGameLogTurnAsNode.find('p').html(pTwoTextLog)
         $currentGameLogTurnAsNode.css('zIndex','251').show()
-        console.log('loaded');
         
         // reset opacity of background color in all .playingSquare to 0. 
         $('.playingSquare').css("backgroundColor", "rgba(255, 0, 0, 0)")   
@@ -200,11 +213,12 @@ $('.playingSquare').on('click', function ( ) {          //              onSquare
 
         // set gameBoard reference to occupied by playerTwo.
         gameBoard[id] = 'O';
-        console.log(gameBoard); //    check = true
+        // console.log(gameBoard); //    check = true
     
-        // checks for win condition - if no winner, switch player priority from playerTwo to playerOne.
+        // checkWin function: if no winner, switch player priority
         checkWin();
-        console.log(playerWithPriority); //     check = true    
+        // console.log(playerWithPriority); //     check = true  
+        // add 1 to turnCounter  
         turnCounter++ ;
 
     } else {
@@ -217,67 +231,84 @@ $('.playingSquare').on('click', function ( ) {          //              onSquare
 
 });                                                     //       closes onSquareClick eventListener
 
-let gameRulesShowing = true
 // eventListener to 'Open or Close' gameRules
 $('#rulesButton').on('click' , function(){
+    
     // check to see if gameRules are showing
     if ( gameRulesShowing ) {
-        // initates .css changes to #gameRules        
+
+        // initates if .css changes to #gameRules        
         $('#gameRules').animate({
             height : '2vh',
             top: '1vh'
         }, 3000, function(){
-        })
+        })//closes if .css changes to #gameRules
+
+        // switches gameRulesShowing value
+        gameRulesShowing = false
+    } else {
+
+        // initates else .css changes to #gameRules
+        $('#gameRules').animate({
+           height : '25vh',
+ 
+        },3000, function(){
+
+        })//closes else .css changes to #gameRules
+
+        // switches gameRulesShowing value
+        gameRulesShowing = true
+
+    } //    closes if/else tree   
+
+}) //       closes eventListener for gameRules button
+
+// eventListener to select Game Mode: ORIGINAL
+$('#original').on('click' , function(){
+    
+    // bring gameLog to front
+    $('#gameLog')
+    .animate({ width : '52.3vw', zIndex: '75'}, 3000 ) 
+        
+    // if condition for gamesRules
+    if ( gameRulesShowing ) {
+        // initates .css changes to #gameRules        
+        $('#gameRules')
+        .animate({ height : '2.5vh', top: '1.3vh', textAlign: 'left'}, 3000 )
+        $('#heading')
+        .animate({ margin: '0 0 4vh 0' }, 3000 )
+        $('#rulesText')
+        .animate({ left: '-20vw' }, 3000)
+        
         // switches gameRulesShowing value
         gameRulesShowing = false
     } else {
         // initates .css changes to #gameRules
         $('#gameRules').animate({
-           height : '25vh',
- 
+            height : '25vh',
+            top: '1vh',
+            
         },3000, function(){
         })
+
         // switches gameRulesShowing value
         gameRulesShowing = true
-    }
-})
 
-// eventListener to select Game Mode
-$('#original').on('click' , function(){
-    
-        $('#gameLog')
-        .animate({ width : '52.3vw', zIndex: '75'}, 3000 ) 
-        
-        if ( gameRulesShowing ) {
-            // initates .css changes to #gameRules        
-            $('#gameRules')
-            .animate({ height : '2.5vh', top: '1.3vh'}, 3000 )
-            $('#heading')
-            .animate({ margin: '0 0 6.5vh 0' }, 3000 )
-            
-            // switches gameRulesShowing value
-            gameRulesShowing = false
-        } else {
-            // initates .css changes to #gameRules
-            $('#gameRules').animate({
-               height : '25vh',
-               top: '1vh'
-            },3000, function(){
-            })
-            // switches gameRulesShowing value
-            gameRulesShowing = true
-        }        
-});
+    }//     closes if/else tree       
+}); //      closes eventListener-GameMode:ORIGINAL
 
+// eventListener to select Game Mode: OTHER
 $('#other').on('click', function (){
+    // bring gameLog to front
     $('#gameLog').css('zIndex' , 75 )
 
+    // reposition gameLog area
     $('#gameLog')
       .animate( { width : '110vw' }, 200 )
       .animate( { width : '43vw' }, 500 )
       .animate( { width : '52.3vw' }, 2000);
 
-
+    // if condition for gameRules
     if ( gameRulesShowing ) {
         // initates .css changes to #gameRules        
         $('#gameRules')
@@ -285,11 +316,13 @@ $('#other').on('click', function (){
         .animate({ height : '1vh'  , top: '5vh'}, 500)
         .animate({ height : '2.5vh', top: '1.3vh'}, 2000);
 
+        // initates .css changes to #heading
         $('#heading')
         .animate({ margin: '2.5vh 0 0 0' }, 200)
         .animate({ margin: '0 0 8vh 0'   }, 500)
         .animate({ margin: '0 0 7vh 0'   }, 2000);
         
+        // aligns #rulesText to the left
         $('#rulesText').css('textAlign' , 'left')
         
         // switches gameRulesShowing value
@@ -303,13 +336,11 @@ $('#other').on('click', function (){
         })
         // switches gameRulesShowing value
         gameRulesShowing = true
-    }
-    
-    // $('#gameLog').css('zIndex' , 75 )
+    } //    closses if/else
 
     
 
-});
+}); //      closes eventListener-GameMode:OTHER
 
 
 
